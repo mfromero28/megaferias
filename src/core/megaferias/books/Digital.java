c987febaf6e510e7e3d67e9c449aea5d7aff9791
Maria Fernanda Romero Alvarado <fmalvarado@uninorte.edu.co>
2024-03-08 10:15:40 -0500

    class creation

--- src/core/megaferias/books/Digital.java
+++ src/core/megaferias/books/Digital.java
@@ -2,14 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package main;
+package core.megaferias.books;
 
 /**
  *
  * @author fmalvarado
  */
-public class Main {
-    public static void main(String[] args) {
-        
-    }
+public class Digital {
+    
 }
